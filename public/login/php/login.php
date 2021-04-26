c0473781fe80372007dab99743cc4b0da35bf059
andres2000n <morenoandres761@gmail.com>
2021-04-26 12:00:49 -0500

    validacion login

--- public/login/php/login.php
+++ public/login/php/login.php
@@ -1,5 +1,30 @@
 <?php
 
+    include("conexion.php");
 
+    if (isset($_POST['enviar'])) {
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+
+        $consul = "SELECT * FROM usuario WHERE documento = '$user' AND clave = '$pass'";
+        $query = mysqli_query($mysqli, $consul);
+        $fila = mysqli_fetch_assoc($query);
+
+        if ($fila) {
+            $_SESSION['tipo'] = $fila['id_tip_usu'];
+            $_SESSION['nom'] = $fila['nombre'];
+            $_SESSION['ape'] = $fila['apellido'];
+            $_SESSION['pass'] = $fila['clave'];
+
+            if ($_SESSION['pass'] == $pass) {
+                header("Location: ../../administrador/administrador.php");
+                // echo 'ta bien';
+            } else {
+                echo 'este usuario es invalido';
+            }
+        }
+    } else {
+        echo 'hay un error';
+    }
 
 ?>
