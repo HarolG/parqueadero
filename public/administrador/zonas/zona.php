ec5a68994c672423bb47055a8a96b7c7764448dd
jhoan301 <jdsanchez3019@misena.edu.co>
2021-06-01 09:41:56 -0500

    a toda costa

--- public/administrador/zonas/zona.php
+++ public/administrador/zonas/zona.php
@@ -5,104 +5,115 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../../../img/logo.ico"/>
-    <title>Crear Zonas</title>
-    <!-- Estilos Generales -->
-    <link rel="stylesheet" href="../../../layout/css/navegacion.css">
-    <link rel="stylesheet" href="css/main.css">
-    <!-- Font awesome -->
-    <script src="https://kit.fontawesome.com/a90c49b6b2.js" crossorigin="anonymous"></script>
-    <!-- Tipo de letra -->
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400&display=swap" rel="stylesheet">
-    <script src="js/validar.js"></script>
+	<title>Inicio</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="icon" href="../../../img/logo.ico"/>
+    <!-- estilos generales -->
+	<link rel="stylesheet" href="../../../layout/css/main.css">
+	<link rel="stylesheet" href="css/main.css">
+	<!-- Tipo de letra -->
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400&display=swap"
+		rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a90c49b6b2.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container">
-    <div class="navegacion">
-            <div class="site_title">
-                <img src="../../../img/Logo_parking_2.0.png" alt="logo" class="logo">
-            </div>
-            <div class="nav_profile">
-                <div class="profile_pic">
-                    <img src="../../../img/foto_perfil.png" alt="">
-                </div>
-                <div class="profile_info">
-                    <div>
-                        <p class="profile_welcome">Bienvenido,</p>
-                        <p class="profile_name"><?php echo $_SESSION['nom']," ", $_SESSION['ape']?></p>
-                    </div>
-                </div>
-            </div>
-            <div class="menu_section">
-                <h5>GENERAL</h5>
-                <ul>
-                    <li>
-                        <a href="../home/administrador.php"><i class="fa fa-home " aria-hidden="true"> Inicio </i></a>
-                        <div class="nav_decorate"></div>
-                    </li>
-                    <li>
-                        <a href="../parqueo/parqueo.php"> <i class="fa fa-sign-in-alt" aria-hidden="true"> Reporte de Entradas </i></i></a>
-                        <div class="nav_decorate"></div>
-                    </li>
-                    <li>
-                        <a href="../zonas/zona.php"><i class="fa fa-plus" aria-hidden="true"> Crear Zonas </i></a>
-                        <div class="nav_decorate"></div>
-                    </li>
-                    <li>
-                        <a href="../usuarios/usuarios.php"><i class="fa fa-users" aria-hidden="true"> Crear Usuarios </i></a>
-                        <div class="nav_decorate"></div>
-                    </li>
-                    <li>
-                        <a href="../crear/crearusu.php"> <i class="fa fa-car" aria-hidden="true"> Registrar Vehiculos </i></i></a>
-                        <div class="nav_decorate"></div>
-                    </li>
-                </ul>
-            </div>
-            <!-- <div class="menu_section">
-                <h5>PROXIMAMENTE</h5>
-                <ul>
-                    <li>
-                        <a href="#"><i class="fa fa-chart-pie " aria-hidden="true"> Informes </i></a>
-                        <div class="nav_decorate"></div>
-                    </li>
+	<!-- SideBar -->
+	<section class="full-box cover dashboard-sideBar">
+		<div class="full-box dashboard-sideBar-bg btn-menu-dashboard"></div>
+		<div class="full-box dashboard-sideBar-ct">
+			<!--SideBar Title -->
+			<div class="full-box text-uppercase text-center text-titles dashboard-sideBar-title">
+				<img src="../../../img/Logo_parking_2.0.png" alt="logo" class="logo" style="width: 150px; height: 70px; display: flex; justify-content: center; margin-left:40px;">
+			</div>
+			<!-- SideBar User info -->
+			<div class="full-box dashboard-sideBar-UserInfo">
+				<figure class="full-box">
+					<img src="../../../img/foto_perfil.png" alt="UserIcon">
+					<div class="text-center text-titles">
+						<p class="profile_welcome">Bienvenido,</p>
+						<p class="profile_name">
+							<?php echo $_SESSION['nom']," ", $_SESSION['ape']?>
+						</p>
+					</div>
+				
+				</figure>
+				
+				<ul class="full-box list-unstyled text-center">
+					<li>
+						<a href="#!">
+							<i class="fas fa-cogs"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#" class="btn-exit-system">
+							<i class="fas fa-power-off"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- SideBar Menu -->
+			<ul class="list-unstyled full-box dashboard-sideBar-Menu">
+				<li>
+					<a href="../home/administrador.php">
+						<i class="fas fa-home"></i> Inicio 
+					</a>
+				</li>
+				<li>
+					<a href="../parqueo/parqueo.php" class="btn-sideBar-SubMenu">
+						<i class="fa fa-sign-in-alt" aria-hidden="true"></i> Reporte de entradas
+					</a>
+					
+				</li>
+				<li>
+					<a href="zona.php" class="btn-sideBar-SubMenu">
+						<i class="fa fa-plus" aria-hidden="true"></i> Crear zonas 
+					</a>
+					
+				</li>
+				<li>
+					<a href="../usuarios/usuarios.php" class="btn-sideBar-SubMenu">
+						<i class="fa fa-users" aria-hidden="true"></i> Crear usuarios 
+					</a>
+					
+				</li>
+				<li>
+					<a href="../crear/crearusu.php" class="btn-sideBar-SubMenu">
+						<i class="fa fa-car" aria-hidden="true"></i> Registro de vehiculos
+					</a>
+					
+				</li>
+			</ul>
+		</div>
+	</section>
 
-                </ul>
-            </div> -->
-        </div>
-        <div class="contenido">
-            <div class="nav_menu">
-                <div class="container_nav-menu">
-                    <div class="icon_menu">
-                        <i class="fa fa-bars" aria-hidden="true"></i>
-                    </div>
-                    <div class="enlaces">
-                        <ul>
-                            <li><strong><a href="http://centrodeindustria.blogspot.com">Centro de Industria y Construcción</a></strong></li>
-                            <li><strong><a href="http://oferta.senasofiaplus.edu.co/sofia-oferta/">Portal de Sofia Plus</a></strong></li>
-                        </ul>
-                    </div>
-                    <div class="user">
-                        <div class="user_pic">
-                            <img src="../../../img/foto_perfil.png" alt="">
-                        </div>
-                            <ul class="navy">
-                                <li>
-                                    <a href=""><p><?php echo $_SESSION['nom']," ", $_SESSION['ape']?><i class="fa fa-caret-down" aria-hidden="true"></i></p></a>
-                                    <ul>
-                                        <li><a href="">Your Profile</a></li>
-                                        <li><a href="">Settings</a></li>
-                                        <li><a href="../../../php/cerrarsesion.php">Logout</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                    </div>
-                </div>
-           </div>
+	<!-- barra de menus-->
+	<section class="full-box dashboard-contentPage">
+		<!-- NavBar -->
+		<nav class="full-box dashboard-Navbar">
+			<ul class="full-box list-unstyled text-right">
+				<li class="pull-left">
+					<a href="#!" class="btn-menu-dashboard"><i class="fa fa-bars" aria-hidden="true"></i></a>
+				</li>
+				<li>
+					<a href="#!" class="btn-Notifications-area">
+						<i class="far fa-envelope"></i>
+						<span class="badge">7</span>
+					</a>
+				</li>
+				<li>
+					<a href="#!" class="btn-modal-help">
+						<i class="far fa-question-circle"></i>
+					</a>
+				</li>
+                <a class="pull-left" style="width: 250px;" href="http://centrodeindustria.blogspot.com">Centro de Industria y Construcción</a>   
+               
+                <a class="pull-left" style="width: 170px;"  href="http://oferta.senasofiaplus.edu.co/sofia-oferta/">Portal de Sofia Plus</a>
+			</ul>    
+		</nav>
            <!-- Aquí va el contenido -->
         <form action="php/crear_zona.php" id="form" method="POST" onsubmit="return validar();">
             <h2 class="titulo">CREAR ZONAS</h2>
@@ -181,9 +192,102 @@
         </div>
     </div>
 
+  </section>
+
+	<!-- Notifications area -->
+
+	<section class="full-box Notifications-area">
+		<div class="full-box Notifications-bg btn-Notifications-area"></div>
+		<div class="full-box Notifications-body">
+			<div class="Notifications-body-title text-titles text-center">
+				Notifications <i class="fas fa-times-circle btn-Notifications-area"></i>
+			</div>
+			<div class="list-group">
+			  	<div class="list-group-item">
+				    <div class="row-action-primary">
+				      	<i class="zmdi zmdi-alert-triangle"></i>
+				    </div>
+				    <div class="row-content">
+				      	<div class="least-content">17m</div>
+				      	<h4 class="list-group-item-heading">Tile with a label</h4>
+				      	<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus.</p>
+				    </div>
+			  	</div>
+			  	<div class="list-group-separator"></div>
+			  	<div class="list-group-item">
+				    <div class="row-action-primary">
+				      	<i class="zmdi zmdi-alert-octagon"></i>
+				    </div>
+				    <div class="row-content">
+				      	<div class="least-content">15m</div>
+				      	<h4 class="list-group-item-heading">Tile with a label</h4>
+				      	<p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus.</p>
+				    </div>
+			  	</div>
+			  	<div class="list-group-separator"></div>
+				<div class="list-group-item">
+				    <div class="row-action-primary">
+				      	<i class="zmdi zmdi-help"></i>
+				    </div>
+				    <div class="row-content">
+				      	<div class="least-content">10m</div>
+				      	<h4 class="list-group-item-heading">Tile with a label</h4>
+				      	<p class="list-group-item-text">Maecenas sed diam eget risus varius blandit.</p>
+				    </div>
+				</div>
+			  	<div class="list-group-separator"></div>
+			  	<div class="list-group-item">
+				    <div class="row-action-primary">
+				      	<i class="zmdi zmdi-info"></i>
+				    </div>
+				    <div class="row-content">
+				      	<div class="least-content">8m</div>
+				      	<h4 class="list-group-item-heading">Tile with a label</h4>
+				      	<p class="list-group-item-text">Maecenas sed diam eget risus varius blandit.</p>
+				    </div>
+			  	</div>
+			</div>
+
+		</div>
+	</section>
+
+	<!-- Dialog help -->
+	<div class="modal fade" tabindex="-1" role="dialog" id="Dialog-Help">
+	  	<div class="modal-dialog" role="document">
+		    <div class="modal-content">
+			    <div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			    	<h4 class="modal-title">Help</h4>
+			    </div>
+			    <div class="modal-body">
+			        <p>
+			        	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt beatae esse velit ipsa sunt incidunt aut voluptas, nihil reiciendis maiores eaque hic vitae saepe voluptatibus. Ratione veritatis a unde autem!
+			        </p>
+			    </div>
+		      	<div class="modal-footer">
+		        	<button type="button" class="btn btn-primary btn-raised" data-dismiss="modal">Ok <i class="fas fa-exclamation"></i></button>
+		      	</div>
+		    </div>
+	  	</div>
+	</div>
+
+</body>
+    <!-- Scripts cambiables -->
     <script src="../../../library/jquery-3.6.0.min.js"></script>
     <script src="js/confirmacion.js"></script>
-</body>
+
+    <!--====== Scripts pagina ¡¡NO CAMBIAR!! -->
+    <script src="../../../layout/js/jquery-3.1.1.min.js"></script>
+    <script src="../../../layout/js/sweetalert2.min.js"></script>
+    <script src="../../../layout/js/bootstrap.min.js"></script>
+    <script src="../../../layout/js/material.min.js"></script>
+    <script src="../../../layout/js/ripples.min.js"></script>
+    <script src="../../../layout/js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="../../../layout/js/main.js"></script>
+    <script>
+        $.material.init();
+    </script>
+
 </html>
 
 <?php
@@ -193,3 +297,4 @@
                 </script>';
     }
 ?>
+
