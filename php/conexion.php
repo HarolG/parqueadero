2df09d57a69f852550e45d707d43791358a92718
jhoan301 <jsanchez.sabogal07@gmail.com>
2021-06-17 14:03:40 -0500

    nuevos cambios

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,15 @@
 <?php
 
+    $server = "localhost";
+	$user = "root";
+	$password = "";//poner tu propia contraseña, si tienes una.
+	$bd = "parqueadero";
+
+	$conexion = mysqli_connect($server, $user, $password, $bd);
+	if (!$conexion){ 
+		die('Error de Conexión: ' . mysqli_connect_errno());	
+	}	
+    
     $mysqli = new mysqli("localhost", "root", "", "parqueadero");
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
---
+++ public/administrador/usuarios/guardar.php
@@ -0,0 +1,58 @@
+<?php
+    include ("../../../php/conexion.php");
+
+
+    $documento=$_POST["documento"];
+    $nombre=$_POST["nombre"];
+    $apellido=$_POST["apellido"];
+    $edad = $_POST["edad"];
+    $celular = $_POST["celular"];
+    $direccion = $_POST["direccion"];
+    $correo = $_POST["correo"];
+    $tipdoc=$_POST["tipdoc"];
+    $tipusu=$_POST["tipusu"];
+    $estausu=$_POST["estausu"];
+    $opcion =$_POST["opcion"];
+    $informacion = [];
+
+    switch ($opcion) {
+        case 'modificar':
+            modificar($edad, $celular, $direccion, $correo, $documento, $conexion);
+            break;
+        
+        case 'eliminar':
+            eliminar($documento, $conexion);
+            break;
+    }
+
+
+    function modificar($edad, $celular, $direccion, $correo, $documento, $conexion){ 
+        $query= "UPDATE usuario SET edad='$edad',
+                                    celular='$celular',
+                                    direccion='$direccion',
+                                    correo='$correo' 
+                                WHERE documento = '$documento'";
+        $resultado = mysqli_query($conexion, $query);
+        verificar_resultado( $resultado );
+        cerrar( $conexion );
+    }
+
+    function eliminar($documento, $conexion){
+        $query = "DELETE FROM usuario WHERE documento = $documento";
+        $resultado = mysqli_query($conexion, $query);
+        verificar_resultado( $resultado );
+        cerrar( $conexion );
+    }
+
+
+
+    function verificar_resultado($resultado){
+        if (!$resultado) $informacion["respuesta"]="ERROR!";
+        else $informacion["respuesta"]="BIEN!";
+        echo json_encode($informacion);
+    }
+
+    function cerrar($conexion){
+        mysqli_close($conexion);
+    }
+?>
